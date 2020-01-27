7c97b0964b568b107ece9494c63815d8558cc1aa
vai na web <vainawebcpx@gmail.com>
2020-01-27 17:42:46 -0300

    novos arquivos criados

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      modOpen: false,
+    }
+  }
+
+  openModal = (event) => {
+    event.preventDefault()
+    this.setState({
+      modOpen: true,
+    })
+  }
+
+  renderModal = () => (
+    <div className='overlay'>
+      <div className='container-modal'>
+        <p>para fechar clique no botão abaixo</p>
+        <button onClick={() => this.setState({modOpen: false})}>fechar modal</button>
+      </div>
     </div>
-  );
-}
+  )
+
+  render() {
+    return (
+      <div className="App">
+        <form className='box-form' onSubmit={this.openModal}>
+          <p>o modal só abre clicando no botão</p>
+          <button>abrir modal</button>
+        </form>
+        {this.state.modOpen ? this.renderModal() : null}
+      </div>
+    );
+  }
+  }
 
 export default App;
